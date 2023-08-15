eb2c4cd186a9c1249678e7dc6b87527a46435668
Sanjay <sanjayacharya992@.com>
2023-08-15 16:11:37 +0530

    motion div and responsive tailwindConfig

--- client/src/component/Post.jsx
+++ client/src/component/Post.jsx
@@ -13,7 +13,16 @@ const Post = () => {
     fun()
   },[])
   return (
-    <div className='h-[300px] w-[300px] bg-black m-5'>Post</div>
+    // <div className='h-[300px] w-[300px] bg-black m-5 text-white'>Post</div>
+    <div className="flex mb-2 lg:order-2 ">
+              <div className="relative overflow-hidden w-48 h-48 lg:w-72 lg:h-72 bg-gradient-to-tr from-orange-400 to-green-400 rounded-blob animate-san">
+                {/* <img
+                  src={profile}
+                  alt=""
+                  className="absolute top-20 -left-3 lg:top-28 lg:-left-5 scale-[1.75]"
+                /> */}
+              </div>
+            </div>
   )
 }
 
